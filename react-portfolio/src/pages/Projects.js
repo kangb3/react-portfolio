812f0755d89f258b274c60f01aa38d0d5a6458da
Baldeep Kang <bkang4612@gmail.com>
2020-11-15 19:52:21 -0800

    added Carousel element

--- react-portfolio/src/pages/Projects.js
+++ react-portfolio/src/pages/Projects.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Carousel from '../components/Carousel';
 import Jumbo from '../components/Jumbo';
 
 function Project(props){
     //console.log(props)
 
     return(
-        <Jumbo title={props.title} />
+        <div>
+
+            <Jumbo title={props.title}  />
+            <Carousel />
+
+        </div>
     );
 
 }
