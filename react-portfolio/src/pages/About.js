ac62a4ca047a9165f9147dc9a8174aae5b8a9f94
Baldeep Kang <bkang4612@gmail.com>
2020-11-16 00:18:10 -0800

    include Content component for About page

--- react-portfolio/src/pages/About.js
+++ react-portfolio/src/pages/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Content from '../components/Content';
+import Jumbo from '../components/Jumbo';
 
 function About(props){
 
     return(
-        <p>About</p>
+        <div>
+            <Jumbo title={props.title} />
+
+            <Content>
+                <p>My name is Baldeep Kang. Currently, I am a System Administrator for the State of Washington.</p>
+
+                <p>I graduated from University of Washington Tacoma with Bachelors degree in information Technology.</p>
+
+                <p>I am enrolled in Bootcamp offered by University of Washington. I want to gain new skills in order to advance my career.</p>
+            </Content>
+        </div>
     )
 
 }
